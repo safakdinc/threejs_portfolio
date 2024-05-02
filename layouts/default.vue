5b0f7a18f8aaeeb268652084950863f55d03e4aa
Şafak Dinç <114588752+kodsilici02@users.noreply.github.com>
2024-05-03 00:48:33 +0300

    I was in a hurry when i made the previous commit.

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,16 @@
 <template>
   <div class="w-full">
-    <div class="fixed bottom-0 right-0 p-[24px] px-[32px] flex gap-[16px] z-[2000]">
-      <a href="https://www.linkedin.com/in/şafak-dinç-7676652b0/" target="_blank" class="w-7 h-7 icon cursor-pointer">
-        <IconLinkedin></IconLinkedin>
-      </a>
-      <a href="https://github.com/kodsilici02" target="_blank" class="w-7 h-7 icon cursor-pointer">
-        <IconGithub></IconGithub>
-      </a>
+    <div class="fixed bottom-0 right-0 p-[24px] px- flex gap-[16px] z-[2000]">
+      <div class="w-7 h-7 icon">
+        <a href="https://www.linkedin.com/in/şafak-dinç-7676652b0/" target="_blank" class="cursor-pointer hover-effect">
+          <IconLinkedin></IconLinkedin>
+        </a>
+      </div>
+      <div class="w-7 h-7 icon">
+        <a href="https://github.com/kodsilici02" target="_blank" class="cursor-pointer hover-effect">
+          <IconGithub></IconGithub>
+        </a>
+      </div>
     </div>
     <!--NavBar-->
     <NavBar></NavBar>
@@ -18,8 +22,9 @@
 <script setup></script>
 
 <style lang="scss" scoped>
-.icon {
+.hover-effect {
   transition: transform 0.2s ease-out;
+  transform: scale(1);
   &:hover {
     transform: scale(1.1);
   }
