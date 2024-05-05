5c32127516e6934a68c783c644c59f9c2da40a2d
Şafak Dinç <114588752+kodsilici02@users.noreply.github.com>
2024-05-05 19:34:53 +0300

    logo optimized

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -4,7 +4,7 @@
       <div class="absolute top-0 left-0 w-full h-full navbar z-[999]" ref="navbar"></div>
       <div class="col-start-0 lg:col-start-3 px-5 lg:px-3 col-span-12 lg:col-span-8 flex justify-between py-2 items-center z-[1000]">
         <div class="flex h-full items-center logo">
-          <img src="https://i.ibb.co/kGLFNbn/IMG-1683.png" class="rounded-full object-cover object-center h-[60px]" />
+          <img src="https://i.ibb.co/jRrmZQt/panter.png" class="rounded-full object-cover object-center h-[60px]" />
           <div class="lg:text-2xl md-text font-bold hidden sm:block">Yazılım Panteri</div>
         </div>
         <div class="flex">
