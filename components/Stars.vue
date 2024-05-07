406e8d8dbadf9619f2946c7bdf4a87a1a5dc2f9c
Şafak Dinç <114588752+kodsilici02@users.noreply.github.com>
2024-05-07 18:36:23 +0300

    stars scene background matched with page background

--- components/Stars.vue
+++ components/Stars.vue
@@ -11,7 +11,7 @@ const canvasContainer = ref(null);
 onMounted(() => {
   // Setup Three.js scene
   const scene = new THREE.Scene();
-  scene.background = new THREE.Color(0x000000);
+  scene.background = new THREE.Color(0x030303);
   const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
   camera.position.z = 1;
 
